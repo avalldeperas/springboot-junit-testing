42b31799302b56a07f207d7be957bc507700d774
Albert Valldeperas <albertvalldeperas@gmail.com>
2020-07-21 18:29:12 +0200

    adds two new tests: multiple argument catching and spy/mock difference

--- src/test/java/com/avalldeperas/springbootjunittesting/unitesting/business/ListMockTest.java
+++ src/test/java/com/avalldeperas/springbootjunittesting/unitesting/business/ListMockTest.java
@@ -1,11 +1,10 @@
 package com.avalldeperas.springbootjunittesting.unitesting.business;
 
-import org.apache.juli.logging.Log;
-import org.apache.juli.logging.LogFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
-import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -65,11 +64,54 @@ public class ListMockTest {
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         verify(mock).add(captor.capture());
 
-        log.info("captor capture = " + captor.capture());
-        log.info("captor value = " + captor.getValue());
-
         assertEquals("SomeString", captor.getValue());
+    }
 
+    @Test
+    public void multipleArgumentCapturing() {
+        mock.add("SomeString1");
+        mock.add("SomeString2");
+
+        // verification of which argument has been passed
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock, times(2)).add(captor.capture());
+
+        List<String> allValues = captor.getAllValues();
+
+        assertEquals("SomeString1", allValues.get(0));
+        assertEquals("SomeString2", allValues.get(1));
+    }
+
+    /**
+     * A mock does not retain behaviour (code) of the original class.
+     * However, a spy retains behaviour of the original class by default.
+     */
+    @Test
+    public void spying() {
+        // mock
+        ArrayList arrayListMock = mock(ArrayList.class);
+        log.info("array get(0) = " + arrayListMock.get(0)); // no error bc mock
+        log.info("array size = " + arrayListMock.size());
+        arrayListMock.add("String");
+        log.info("array size = "+arrayListMock.size());
+
+        // spy
+        ArrayList arrayListSpy = spy(ArrayList.class);
+        arrayListSpy.add("Test0");
+        log.info("array get(0) = " + arrayListSpy.get(0));
+        log.info("array size = "+arrayListSpy.size());
+        arrayListSpy.add("String");
+        log.info("array size = "+arrayListSpy.size());
+
+        // mocking a spy - from now on, size is always 5
+        when(arrayListSpy.size()).thenReturn(5);
+        log.info("array size = " + arrayListSpy.size());
+
+        arrayListSpy.add("Test4");
+        log.info("array size = " + arrayListSpy.size()); // 5 anyway
+
+        boolean hasAddedTest4 = verify(arrayListSpy).add("Test4");
+        log.info("have we added Test4 string? " + hasAddedTest4);
     }
 
 }
