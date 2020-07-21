5a5cc7a4935861c942cfab821031b1b4c2cc03fc
Albert Valldeperas <albertvalldeperas@gmail.com>
2020-07-21 14:02:40 +0200

    final refactoring of business test by using MockitoExtension

--- src/test/java/com/avalldeperas/springbootjunittesting/unitesting/business/SomeBusinessMockTest.java
+++ src/test/java/com/avalldeperas/springbootjunittesting/unitesting/business/SomeBusinessMockTest.java
@@ -2,22 +2,23 @@ package com.avalldeperas.springbootjunittesting.unitesting.business;
 
 import com.avalldeperas.springbootjunittesting.unittesting.business.SomeBusinessImpl;
 import com.avalldeperas.springbootjunittesting.unittesting.data.SomeDataService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessMockTest {
 
-    private SomeBusinessImpl business = new SomeBusinessImpl();
-    private SomeDataService dataServiceMock = mock(SomeDataService.class);
+    @InjectMocks
+    private SomeBusinessImpl business;
 
-    @BeforeEach
-    public void before() {
-        business.setSomeDataService(dataServiceMock);
-    }
+    @Mock
+    private SomeDataService dataServiceMock;
 
     @Test
     public void calculateSumUsingDataService_basic() {
