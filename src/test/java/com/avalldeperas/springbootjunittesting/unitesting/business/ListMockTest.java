5e463cb6f460de830b6e91220adcae8fed44ff31
Albert Valldeperas <albertvalldeperas@gmail.com>
2020-07-21 14:12:05 +0200

    add a mock to test list interface returning different values and parameters

---
+++ src/test/java/com/avalldeperas/springbootjunittesting/unitesting/business/ListMockTest.java
@@ -0,0 +1,35 @@
+package com.avalldeperas.springbootjunittesting.unitesting.business;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListMockTest {
+
+    List mock = mock(List.class);
+
+
+    @Test
+    public void size_basic() {
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void returnDifferentValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters() {
+        when(mock.get(0)).thenReturn("firstString");
+        assertEquals("firstString", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+}
