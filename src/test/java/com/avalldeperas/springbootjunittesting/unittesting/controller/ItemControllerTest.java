fb8ec40fcf68b4c6dac741bf64f38ceb5b809715
avalldeperas <albertvalldeperas@gmail.com>
2020-07-22 18:07:22 +0200

    adds unit test to the controller layer while mocking the business layer

--- src/test/java/com/avalldeperas/springbootjunittesting/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/avalldeperas/springbootjunittesting/unittesting/controller/ItemControllerTest.java
@@ -13,7 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -57,6 +60,25 @@ public class ItemControllerTest {
         MvcResult result = mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().json("{id:2, name:Item2, price:10, quantity:10}"))
+                .andReturn();
+    }
+
+    @Test
+    public void retrieveAllItems_basic() throws Exception {
+        when(businessService.retrieveAllItems()).thenReturn(
+                Arrays.asList(
+                        new Item(2, "Item2", 10, 10),
+                        new Item(3, "Item3", 20, 50)
+                )
+        );
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/all-items-from-database")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{id:2, name:Item2, price:10, quantity:10}, " +
+                        "{id:3, name:Item3, price:20, quantity:50}]"))
                 .andReturn();
     }
 }
